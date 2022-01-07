dd671d5cd6531ee12eb6a0424b1b4754060de83f
Shiraz08 <65015611+Shiraz08@users.noreply.github.com>
2022-01-07 16:09:32 +0500

    Model Update

---
+++ CRM/Models/CRMModel/ACCOUNTS.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.Models.CRMModel
+{
+	public class ACCOUNTS
+	{
+		[Key]
+		public string ACC_CODE{ get; set; }
+		public string ACC_NAME{ get; set; }
+		public string ACC_TYPE{ get; set; }
+		public string ACC_LINK{ get; set; }
+		public bool ACC_CHK{ get; set; }
+		public string USR_NA{ get; set; }
+		public string ACC_MASK{ get; set; }
+		public string UPD_CHK{ get; set; }
+		public bool NNR{ get; set; }
+		public double Op_Bal{ get; set; }
+		public double OP_DAMT{ get; set; }
+		public double OP_CAMT{ get; set; }
+	}
+}
---
+++ CRM/Models/CRMModel/AccountsZone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.Models.CRMModel
+{
+    public class AccountsZone
+    {
+        [Key]
+        public long ZoneId{ get; set; }
+        public string ZoneDescription{ get; set; }
+        public string ZoneSuperVisor{ get; set; }
+    }
+}
